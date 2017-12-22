f391c343d7dd62dcf62d0ed2d487f56a25a06342
Raphael Hardt Linux <raphael.hardt@gmail.com>
2017-12-22 10:50:06 -0200

    novo interpreter: palavra

---
+++ CafeBot/MeFala/Interpreters/PalavraInterpreter.js
@@ -0,0 +1,20 @@
+
+/**
+ * Fala a palavra que a pessoa pediu.
+ * Só aceita no máximo 3 palavras, e o total de caracteres tem que ser até 25.
+ * Esses limites são pra não quebrar o bot.
+ *
+ */
+module.exports = class PalavraInterpreter {
+    constructor() {}
+
+    static interpret(user, questionPhrase, mentions) {
+        return questionPhrase.match(/^[\w\s]{1,3}$/g) && questionPhrase.length <= 25;
+    }
+
+    static get priority() { return 5 };
+
+    static phrases(user, questionPhrase, mentions) {
+        return [questionPhrase.toString()];
+    }
+};
